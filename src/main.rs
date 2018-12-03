256f7cfd2cb0a347ecac96fb77a3e82473c26b23
Eric Tzeng <eric.s.tzeng@gmail.com>
2018-12-03 14:30:09 -0800

    Add argument parsing to select which day to run

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
+#[macro_use]
+extern crate clap;
 extern crate advent2018;
 
+use clap::{App, Arg};
+
 fn main() {
-    advent2018::day01::solve();
+    let matches = App::new("Advent of Code 2018")
+        .arg(Arg::with_name("day")
+             .short("d")
+             .long("day")
+             .takes_value(true))
+        .get_matches();
+
+    let day = value_t!(matches.value_of("day"), i32).unwrap_or(-1);
+    match day {
+        1 | -1 => advent2018::day01::solve(),
+        _ => {
+            println!("Unknown day: {}", day);
+            std::process::exit(1);
+        }
+    }
 }
